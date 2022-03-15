ca2b055467797a6cb3ee8e6ea318a3efe79999d4
duperrsi <simon.duperray@reseau.eseo.fr>
2022-03-15 13:38:38 +0100

    :tada: init project

---
+++ app/Controllers/Http/AgendaController.ts
@@ -0,0 +1,9 @@
+// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class AgendaController {
+   async index () {
+      return {
+         'key': 'value'
+      }
+   }
+}
